12f0ab84db14b6b47d78885b30608d4ec8996e59
David Rodriguez Hernandez <david.rodriguez@enti.cat>
2020-03-04 13:48:15 +0100

    bullet creada y funcional. Falta acabar destroy bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Transform posicionInicialKunai;
+    public GameObject TexturesKunai;
+    // Start is called before the first frame update
+   
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+    if (Input.GetButtonDown("Fire1"))
+    {
+         Instantiate(TexturesKunai, posicionInicialKunai.position, posicionInicialKunai.rotation);
+    }
+    }
+}
+
+ /*   void PlayerShooting()
+    {
+   
+
+}*/
