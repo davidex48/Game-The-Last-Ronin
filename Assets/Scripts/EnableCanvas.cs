6ac79e8eac551e24f131d1ace96b3b03e3d86fb4
Jan <jan.olcina@enti.cat>
2020-05-20 09:23:14 +0200

    tutorials, menu, dialog

---
+++ Assets/Scripts/EnableCanvas.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableCanvas : MonoBehaviour
+{
+    //The canvas you want to enable
+    public GameObject uiCanvas;
+    void Start()
+    {
+        uiCanvas.SetActive(false);
+    }
+
+    //Check if something has entered the collider this script is on
+    void OnTriggerEnter2D(Collider2D player)
+    {
+
+
+        if (player.gameObject.tag == "Player")
+
+
+        {
+
+            uiCanvas.SetActive(true);
+            StartCoroutine("WaitForSec");
+
+        }
+    }
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(uiCanvas);
+        Destroy(gameObject);
+    }
+}
