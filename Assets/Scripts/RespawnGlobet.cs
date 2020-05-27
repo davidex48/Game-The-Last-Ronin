b5bb1642f6d2d660682ed7cce892396a1c98bfe2
David <david.rodriguez@enti.cat>
2020-05-27 14:01:58 +0200

    Caliz Animation Respawn ok & trees lvl 2 & parallax cambiat

--- Assets/Scripts/RespawnGlobet.cs
+++ Assets/Scripts/RespawnGlobet.cs
@@ -2,26 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Checkpoint : MonoBehaviour
+public class RespawnGlobet : MonoBehaviour
 {
-    public Animator animator;
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player")
+        if (collision.tag == "Player")
         {
-            GameObject.FindGameObjectsWithTag("CheckpointManager")[0].GetComponent<CheckPointManager>().SetPos(this.transform.position); ;
+            //Set animator ON!!!!!!!!!!!!!!!
+            GetComponent<Animator>().SetBool("Flame", true);
         }
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
