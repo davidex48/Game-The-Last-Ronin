dac200b5b2068ced802827b99e1ef25fa5d58b9d
Jan <jan.olcina@enti.cat>
2020-05-27 09:08:57 +0200

    Boss

---
+++ Assets/Audio.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Audio : MonoBehaviour
+{
+
+    public AudioClip Dog;
+    public float Volume;
+    AudioSource audio;
+    public bool alreadyPlayed = false;
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter()
+    {
+        if (!alreadyPlayed)
+        {
+            audio.PlayOneShot(Dog, Volume);
+            alreadyPlayed = true;
+        }
+    }
+}
---
+++ Assets/BossHealth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHealth : MonoBehaviour
+{
+
+    private int BosssHealth = 50;
+
+    public GameObject deathEffect;
+
+    public bool isInvulnerable = false;
+
+    public void damageReceived(int damageValue)
+    {
+        if (isInvulnerable)
+            return;
+
+        BosssHealth -= damageValue;
+        
+
+            if (BosssHealth <= 10)
+        {
+            GetComponent<Animator>().SetBool("IsEnraged", true);
+            GetComponent<ParticleSystem>().Play();
+            ParticleSystem.EmissionModule em = GetComponent<ParticleSystem>().emission;
+            em.enabled = true;
+        }
+
+        if (BosssHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+}
